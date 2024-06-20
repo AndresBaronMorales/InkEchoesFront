d04610fadeafd7f1d9db912cb529d68a923d66f0
Andres Baron <afbaronmorales@gmail.com>
2024-06-20 14:50:55 -0500

    Init FronEnd

---
+++ js/home.js
@@ -0,0 +1,7 @@
+import { navBtn } from './nav.js';
+import { swiperTopBooks } from './homeSwipers.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+    navBtn();
+    swiperTopBooks();
+});
---
+++ js/nav.js
@@ -0,0 +1,9 @@
+export const navBtn = () => {
+    const btnNav = document.getElementById("nav-item-btn");
+
+    btnNav.addEventListener("click", () => {
+        const menuNav = document.getElementById("nav-links");
+        menuNav.classList.toggle("show-links");
+        btnNav.classList.toggle("show-nav");
+    });
+}
